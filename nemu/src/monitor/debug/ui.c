4e274138788b96dc832171251a381084cad6079e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 08:11:50 +0000

    >  compile NEMU 3019244238 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:11:50 up 4 days, 23:02,  1 user,  load average: 0.21, 0.28, 0.23 5e1982e4da2d812e3b35b65ec3073db2a8c344f9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -59,10 +59,10 @@ static int cmd_info(char *args) {
 				printf("%s\t0x%08x\t%d\n",regsl[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
 			puts("");
 			for(i = 0;i < 8; ++i)
-				printf("%s\t0x%04x\t%d\n",regsw[i],cpu.gpr[i]._16,cpu.gpr[i]._16);
+				printf("%s\t0x%8x\t%d\n",regsw[i],cpu.gpr[i]._16,cpu.gpr[i]._16);
 			puts("");
 			for(i = 0;i < 4; ++i)
-				printf("%s\t0x%02x\t%d\n",regsb[i],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);
+				printf("%s\t0x%8x\t%d\n",regsb[i],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);
 			puts("");
 			for(i = 4;i < 8; ++i)
 				printf("%s\t0x%-8x\t%d\n",regsb[i],cpu.gpr[i-4]._8[1],cpu.gpr[i-4]._8[1]);
