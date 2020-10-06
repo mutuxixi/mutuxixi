682b4b354b35186ce8df8ec9ea4021f507d44628
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-06 09:46:31 +0000

    >  compile NEMU 3019244238 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:46:31 up 22:00,  1 user,  load average: 0.38, 0.17, 0.17 4cc7d3862ce16446ac9921f37cb8396bdef0a57f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -183,6 +183,7 @@ static bool make_token(char *e) {
 				break;
 			}
 		}
+assert(0);
 		if(i == NR_REGEX) {
 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
 			return false;
