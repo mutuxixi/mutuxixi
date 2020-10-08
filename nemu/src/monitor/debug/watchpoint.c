e0deab88311f588e53c6922024267a29f2dbbe19
3019244238-Du Yuxuan <vsshandian@163.com>
2020-10-08 15:11:55 +0000

    m6 complete

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,47 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp() {
+	WP *ans;
+	if(free_ == NULL) {
+		printf("Not enoough free wp!\n");
+		assert(0);
+	}
+	ans = free_;
+	free_ = free_ -> next;
+	ans -> next = head;
+	head = ans;
+	return ans;
+}
+
+void free_wp(WP* wp) {
+	WP *tmp, *last;
+	tmp = head;
+	last = NULL;
+	while(tmp != NULL && tmp -> NO != wp -> NO) {
+		last = tmp;
+		tmp = tmp -> next;
+	}
+	if(tmp == NULL) {
+		printf("Error! Not found the wp!\n");
+		assert(0);
+	}
+
+	if(head == wp)
+		head = head -> next;
+	else
+		last -> next = tmp -> next;
+
+	if(free_ == NULL) {
+		free_ = wp;
+		wp -> next = NULL;
+	}
+	else {
+		tmp = free_;
+		while(tmp -> next != NULL)
+			tmp = tmp -> next;
+		tmp -> next = wp;
+		wp -> next = NULL;
+	}
+}
 
