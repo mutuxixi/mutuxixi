084cd75859d88a8369707646d496ef6feaae4ed3
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 02:15:27 +0800

    >  compile NEMU 3019244238 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:15:27 up 8 days,  4:21,  1 user,  load average: 0.94, 0.74, 0.71 46f78edea4bf2edcf780daece6c933b97512eb6f

--- nemu/src/cpu/exec/ret/ret-template.h
+++ nemu/src/cpu/exec/ret/ret-template.h
@@ -4,9 +4,9 @@
 
 make_helper(concat(ret_n_,SUFFIX))
 {
-    cpu.eip = MEM_R(reg_l(R_ESP));
-    reg_l(R_ESP) += DATA_BYTE;
-    print_asm("ret");
+    // cpu.eip = MEM_R(reg_l(R_ESP));
+    // reg_l(R_ESP) += DATA_BYTE;
+    // print_asm("ret");
     return 1;
 }
 
