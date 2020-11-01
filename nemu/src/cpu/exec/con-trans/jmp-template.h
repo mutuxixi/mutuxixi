4795ceb4e2b8d25d3d9906f8a44a4ac44f5d1015
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 19:02:42 +0800

    >  compile NEMU 3019244238 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:02:42 up 1 day, 23:33,  1 user,  load average: 0.36, 0.51, 0.47 c5ea5752d758b1625a7ab04383cf6bebf83dcbb

---
+++ nemu/src/cpu/exec/con-trans/jmp-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jmp
+
+static void do_execute()
+{
+    DATA_TYPE_S displacement = op_src->val;
+    if(op_src->type == OP_TYPE_IMM)
+        cpu.eip += displacement;
+    else
+        cpu.eip = displacement - concat(decode_rm_,SUFFIX)(cpu.eip + 1) - 1;
+    print_asm_template1();
+}
+
+make_instr_helper(i);
+make_instr_helper(rm);
+
+#include "cpu/exec/template-end.h"
