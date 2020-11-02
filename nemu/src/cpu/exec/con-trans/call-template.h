22fdb06d74d6a647c17428f82602acd9391fcf96
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 00:39:17 +0800

    >  compile NEMU 3019244238 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:39:17 up 3 days,  5:09,  1 user,  load average: 0.64, 0.75, 0.79 e1fe777a17c957baf7a06e9ea96ca98a76e84cce

--- nemu/src/cpu/exec/con-trans/call-template.h
+++ nemu/src/cpu/exec/con-trans/call-template.h
@@ -7,7 +7,7 @@ make_helper(concat(call_i_, SUFFIX) )
     int len = concat(decode_i_, SUFFIX) (eip+1);
     reg_l (R_ESP) -= DATA_BYTE;
     MEM_W (reg_l (R_ESP) , cpu.eip + len + 1);
-    DATA_TYPE_S displacement = op_src -> val;
+    DATA_TYPE displacement = op_src -> val;
     print_asm("call 0x%x", cpu.eip + 1 + len + displacement);
     cpu.eip += displacement;
     return len + 1;
@@ -18,9 +18,9 @@ make_helper(concat(call_rm_, SUFFIX) )
     int len = concat(decode_rm_, SUFFIX) (eip+1);
     reg_l (R_ESP) -= DATA_BYTE;
     MEM_W (reg_l (R_ESP) , cpu.eip + len + 1);
-    DATA_TYPE_S displacement = op_src -> val;
+    DATA_TYPE displacement = op_src -> val;
     print_asm("call 0x%x", displacement);
-    cpu.eip += displacement - len -1;
+    cpu.eip = displacement - len -1;
     return len + 1;
 }
 
