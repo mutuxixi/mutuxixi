3aadc34d0583a7524dbe42519ecf8a23313a0dec
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-07 01:49:01 +0800

    >  compile NEMU 3019244238 mutuxixi Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:49:01 up  1:12,  1 user,  load average: 0.25, 0.35, 0.41 5efdbfa6258670a2e1cd05507b57859d9da42a9d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "burst.h"
+#include "memory/cache.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
@@ -6,11 +8,27 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int L1_1st_line = read_cache1(addr);
+	uint32_t offset = addr & (Cache_L1_Block_Size - 1);
+	uint8_t ret[BURST_LEN << 1];
+	if(offset + len > Cache_L1_Block_Size){
+		int L1_2nd_line = read_cache1(addr + Cache_L1_Block_Size - offset);
+		memcpy(ret,cache1[L1_1st_line].data + offset,Cache_L1_Block_Size - offset);
+		memcpy(ret + Cache_L1_Block_Size - offset,cache1[L1_2nd_line].data,len - (Cache_L1_Block_Size - offset));
+	}
+	else{
+		memcpy(ret,cache1[L1_1st_line].data + offset,len);
+	}
+
+	int wtf = 0;
+	uint32_t ans = unalign_rw(ret + wtf, 4) & (~0u >> ((4 - len) << 3));
+	return ans;
+	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
+	write_cache1(addr, len, data);
+	//dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
