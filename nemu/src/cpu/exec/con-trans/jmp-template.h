2751920d892cfa46a27e5b3946020539e8215ad3
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-08 00:29:18 +0800

    >  compile NEMU 3019244238 mutuxixi Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:29:18 up 10:45,  1 user,  load average: 0.73, 0.25, 0.18 f5336fa33d6bb7837959fb6b15c20e162c0f6ef0

--- nemu/src/cpu/exec/con-trans/jmp-template.h
+++ nemu/src/cpu/exec/con-trans/jmp-template.h
@@ -15,4 +15,30 @@ static void do_execute()
 make_instr_helper(i);
 make_instr_helper(rm);
 
+#if DATA_BYTE == 4
+extern SREG_INFO *sreg_info;
+SREG_INFO tmp;
+make_helper(ljmp){
+    sreg_info = &tmp;
+
+    uint32_t op1 = instr_fetch(cpu.eip + 1,4);
+    uint16_t op2 = instr_fetch(cpu.eip + 1 + 4,2);
+    cpu.eip = op1;
+    cpu.cs.selector = op2;
+
+    uint16_t idx = cpu.cs.selector >> 3;
+	lnaddr_t chart_addr = cpu.gdtr.base + (idx << 3);
+	sreg_info->part1 = lnaddr_read(chart_addr, 4);
+	sreg_info->part2 = lnaddr_read(chart_addr + 4, 4);
+
+	cpu.cs.base = (uint32_t)sreg_info->base1 + (sreg_info->base2 << 16) + (sreg_info->base3 << 24);
+	cpu.cs.limit = (uint32_t)sreg_info->limit1 + (sreg_info->limit2 << 16) + (0xfff << 24);
+	if (sreg_info->g == 1) {
+		cpu.cs.limit <<= 12;
+	}
+    print_asm("ljmp 0x%x 0x%x",op2,op1);
+    return 0;
+}
+#endif
+
 #include "cpu/exec/template-end.h"
