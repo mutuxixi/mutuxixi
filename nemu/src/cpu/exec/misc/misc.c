370fb968293151012dec04f028bc73d9eda7ae69
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-09 02:16:37 +0800

    >  compile NEMU 3019244238 mutuxixi Linux ubuntu 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:16:37 up  1:28,  1 user,  load average: 0.76, 0.40, 0.33 8ecd7ab5621e2e58a46dc20c66952753c9f2f161

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -23,3 +23,15 @@ make_helper(lea) {
 	print_asm("leal %s,%%%s", op_src->str, regsl[m.reg]);
 	return 1 + len;
 }
+
+make_helper(cld){
+	cpu.DF = 0;
+	print_asm("cld");
+	return 1;
+}
+
+make_helper(std){
+	cpu.DF = 1;
+	print_asm("std");
+	return 1;
+}
