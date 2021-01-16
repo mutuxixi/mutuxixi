d1fd9d9de2f64a7f2e422544d78d1ac13451e4ee
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 03:31:51 +0800

    >  compile kernel 3019244238 mutuxixi Linux ubuntu 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:31:51 up 7 min,  1 user,  load average: 0.90, 0.97, 0.51 f715147acba53befa3d465686731fcd747bd72d

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -32,14 +32,14 @@ void init_page(void) {
 	/* We use inline assembly here to fill PTEs for efficiency.
 	 * If you do not understand it, refer to the C code below.
 	 */
-
-	// asm volatile ("std;\
-	//  1: stosl;\
-	// 	subl %0, %%eax;\
-	// 	jge 1b;\
-	// 	cld" : :
-	// 	"i"(PAGE_SIZE), "a"((PHY_MEM - PAGE_SIZE) | 0x7), "D"(ptable - 1));
-
+/*
+	asm volatile ("std;\
+	 1: stosl;\
+		subl %0, %%eax;\
+		jge 1b;\
+		cld" : :
+		"i"(PAGE_SIZE), "a"((PHY_MEM - PAGE_SIZE) | 0x7), "D"(ptable - 1));
+*/
 
 	/*
 		===== referenced code for the inline assembly above =====
